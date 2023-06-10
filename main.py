339ac1cb3cf4ce98d1de5327dcb495945fab83ed
shalomavi <shalomavi3@gmail.com>
2023-06-10 12:21:38 +0300

    Fixed bugs

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from movie_app import MovieApp
 from storage_json import StorageJson
 from storage_csv import StorageCsv
+import sys
 
 
 def main():
-    # storage = StorageCsv('data.csv')
-    storage = StorageJson('data.json')
-    movie_app = MovieApp(storage)
-    movie_app.run()
+    try:
+        storage_file = sys.argv[1]
+        if storage_file.split(".")[1] == "csv":
+            storage = StorageCsv(storage_file)
+        else:
+            storage = StorageJson(storage_file)
+
+        movie_app = MovieApp(storage)
+        movie_app.run()
+    except IndexError:
+        storage = StorageCsv('data.csv')
+        storage = StorageJson('data.json')
+        movie_app = MovieApp(storage)
+        movie_app.run()
 
 
 if __name__ == "__main__":
